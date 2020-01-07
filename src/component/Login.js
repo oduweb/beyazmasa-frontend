3483b254209d2dae829ab7180dd9cbaf963d8b1f
proinfinto <furkanyaran@hotmail.com>
2020-01-07 14:36:45 +0300

    Login fixed

--- src/component/Login.js
+++ src/component/Login.js
@@ -3,8 +3,9 @@ import React, { Component } from "react";
 export default class Login extends Component {
   render() {
     return (
-      <div className="flex h-screen">
-        <div className="flex relative content-center flex-wrap bg-white w-screen justify-center">
+      <div className="flex w-screen h-screen">
+        <div className="flex items-center  relative bg-white w-full h-screen">
+          {/* Login Form Area */}
           <div className="px-6 py-4 w-full">
             <div className="text-center text-gray-800 mb-8 font-light h-8 text-lg">
               FRONTEND <span className="font-bold text-xs">V2</span>
@@ -35,29 +36,31 @@ export default class Login extends Component {
                 className="outline-none font-light rounded w-full shadow-lg h-20 p-2 text-xs"
               ></input>
             </div>
+            <div className="text-right font-light text-xs mt-2 ">
+              <button className="inline-flex focus:outline-none">
+                <span>Giriş</span>
+                <img
+                  className="ml-1 mt-090"
+                  width="16px"
+                  alt="Login"
+                  src="https://img.icons8.com/small/100/000000/enter-2.png"
+                ></img>
+              </button>
+            </div>
           </div>
-          <div className="flex-1 outline-none">
-            <button className="inline-flex focus:outline-none float-right mr-6 mb-4 hover:text-gray-500 font-light text-xs">
-              <span>Giriş</span>
-              <img
-                className="ml-1
-                    mt-090
-                    "
-                width="16px"
-                alt="Login"
-                src="https://img.icons8.com/small/100/000000/enter-2.png"
-              ></img>
-            </button>
-          </div>
-          <span className="flex absolute bottom-0 h-8 items-center bg-blue-400 justify-center text-white w-full font-light text-xs font-sans">
+
+          {/* Copyrigt Footer Area */}
+          <div className="inline-flex justify-center absolute items-center bottom-0 h-8 text-center bg-blue-400 text-white w-full font-light text-xs font-sans">
             copyright
-          </span>
+          </div>
         </div>
-        <img
-          className="flex w-8/12 bg-gray-700 object-cover h-screen hidden sm:block"
-          alt="DnA"
-          src={require("../img/windows.jpg")}
-        />
+        <div className="flex-shrink-0 items-center bg-gray-700 h-screen w-8/12 hidden sm:block ">
+          <img
+            className="hidden sm:block object-cover h-screen z-0"
+            alt="DnA"
+            src={require("../img/windows.jpg")}
+          />
+        </div>
       </div>
     );
   }
